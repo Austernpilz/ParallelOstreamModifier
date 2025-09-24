00f65176b05e4278200b6ed9220d03fb1079f84f
Manuel Zschaebitz <manuez42@zedat.fu-berlin.de>
2025-09-24 02:46:40 +0200

    also tests

--- src/UnitTests.cpp
+++ src/UnitTests.cpp
@@ -2,52 +2,92 @@
 #include <thread>
 #include <vector>
 #include <cassert>
-#include "BufferCache.h" 
+#include "Streambuf_Modifier_t.h" 
 
-void singleThreadTest()
+// BufferCache
+void BC_singleThreadTest()
 {
-  BufferCache cache(1, 1024, 2);
+  BufferCache cache(1, 8, 2);
 
-  // get a buffer
+  assert(cache.getLocalCacheSize() == 0)
+  assert(cache.getGlobalCacheSize() == 2)
+
   auto buf1 = cache.getBuffer();
-  assert(buf1->size() == 1024);
+  (*buf1)[0] = static_cast<char>(42);
 
+  auto ptr1 = buf1.get(); // keep raw pointer
   cache.giveBackBuffer(std::move(buf1));
 
   auto buf2 = cache.getBuffer();
-  assert(buf2 != nullptr);
-  assert(buf2->size() == 1024);
+  auto ptr2 = buf2.get();
+
+  // Expect same buffer pointer back from cache
+  assert(ptr1 == ptr2);
+  assert((*buf2)[0] == static_cast<char>(42));
 
   cache.giveBackBuffer(std::move(buf2));
 
-  std::cout << "Single-thread test passed\n";
+  buf1 = cache.getBuffer();
+  auto ptr1 = buf1.get();
+
+  buf2 = cache.getBuffer();
+  auto ptr2 = buf2.get();
+
+  cache.giveBackBuffer(std::move(buf1));
+  cache.giveBackBuffer(std::move(buf2));
+
+   assert(ptr1 != ptr2);
+   assert(cache.getLocalCacheSize() == 2)
+   assert(getGlobalCacheSize() == 0)
+
+  std::cout << "Single-thread cache reuse test passed\n";
 }
 
-void multiThreadTest()
+void BC_multiThreadTest()
 {
   constexpr int thread_count = 4;
-  BufferCache cache(thread_count, 512, 2);
+  BufferCache cache2(thread_count, 64, 2);
 
   std::vector<std::thread> threads;
 
   for (int i = 0; i < thread_count; ++i)
   {
-    threads.emplace_back([&cache, i]()
+    threads.emplace_back( [this] { BC_singleThreadTest(); } )
+    threads.emplace_back( [&cache2, i] ()
     {
-      for (int j = 0; j < 100; ++j)
+      std::unique_ptr<std::vector<char>> first_buf;
+
+      for (int j = 1; j < 50; ++j)
       {
-        auto buf = cache.getBuffer();
-        assert(buf->size() == 512);
-        (*buf)[0] = static_cast<char>(i); // modify to simulate work
-        cache.giveBackBuffer(std::move(buf));
+        auto buf = cache2.getBuffer();
+        if (j == 0)
+        {
+          (*buf)[0] = static_cast<char>(i); // mark with thread id
+          first_buf = std::move(buf);
+          cache2.giveBackBuffer(std::move(first_buf));
+        }
+        else
+        {
+          if ((*buf)[0] == static_cast<char>(i))
+          {
+            // Got our own buffer back
+            std::cout << "Thread " << i << " reused buffer\n";
+            cache2.giveBackBuffer(std::move(buf));
+            return; // success for this thread
+          }
+          cache2.giveBackBuffer(std::move(buf));
+        }
       }
     });
   }
 
-  for (auto& t : threads) t.join();
-
-  std::cout << "Multi-thread test passed\n";
+  for (auto& t : threads) 
+  {
+    if (t.joinable) { t.join(); }
+  }
+  std::cout << "Multi-thread cache reuse test passed\n";
 }
+
 
 int main()
 {
